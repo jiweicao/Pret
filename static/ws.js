2c95b5d5f7fb9e576f74aef55e6997e860c1f3d7
Shaddow <caojiwei1988@gmail.com>
2013-04-02 22:15:57 -0400

    update

---
+++ static/ws.js
@@ -0,0 +1,35 @@
+String.prototype.format = function (){var d=this.toString();if(!arguments.length)return d;var a="string"==typeof arguments[0]?arguments:arguments[0],c;for(c in a)d=d.replace(RegExp("\\{"+c+"\\}","gi"),a[c]);return d};
+
+$(document).ready(function() {
+    if (!window.console) window.console = {};
+    if (!window.console.log) window.console.log = function() {};
+
+    $("#addurl").click(function(){
+        search_modal.init();
+        newMessage({"command":"search", "query":$("#url").val()});
+        return false;
+    });
+});
+
+function newMessage(message) {
+    updater.socket.send(JSON.stringify(message));
+}
+
+var updater = {
+    socket: null,
+    
+    start: function() {
+        var url = "ws://" + location.host + "/ws";
+        if ("WebSocket" in window) {
+	    updater.socket = new WebSocket(url);
+        } else {
+            updater.socket = new MozWebSocket(url);
+        }
+	updater.socket.onmessage = function(event) {
+            msg = JSON.parse(event.data)
+            console.log("Got msg:")
+            console.log(msg)
+            dispathcer[msg.command](msg.arg)
+	}
+    },
+};
